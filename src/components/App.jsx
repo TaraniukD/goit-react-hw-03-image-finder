52f78cdb1c13d44b5d8960c1ae15ab59f9a32ff6
Dmytro <109168390+TaraniukD@users.noreply.github.com>
2023-02-10 09:51:19 +0200

    update components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { Searchbar } from "components/Searchbar/Searchbar";
+import { ImageGallery } from 'components/ImageGallery/ImageGallery';
+import { Button } from "components/Button/Button";
+
+export class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        <Searchbar />
+        <ImageGallery />
+        <Button/>
+      </div>
+    );
+  }
+
 };
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+
+
+export const ImageGallery = () => {
+    return (
+        <ul class="gallery">
+       <ImageGalleryItem />
+       
+      </ul> 
+    )
+}
